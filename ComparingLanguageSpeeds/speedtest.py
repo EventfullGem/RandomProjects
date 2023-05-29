2927d35743f3ac071cbf1808c03a75287a251719
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-05-29 00:04:44 -0400

    Added comments

--- ComparingLanguageSpeeds/speedtest.py
+++ ComparingLanguageSpeeds/speedtest.py
@@ -10,3 +10,5 @@ end_time = time.time()
 print("Time taken:", end_time - start_time, "seconds")
 
 #Time taken: 48.79 seconds
+#So when I run it in Visual Studio IDE and not Visual studio Code I get 7 seconds???
+#I dont know what im doing anymore
