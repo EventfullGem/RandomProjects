6215c1f2c809b6bfbeca6544a3ad5d7aed8843bf
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-05-28 22:15:52 -0400

    Uhh?

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RandomProjects
 Just a random collection of my projects saved to github (feel free to add cool stuff)
+
+I dont really know what I'm doing so Im just going to start messing around 
+
+
+The first folder is StorageGenerator V2
+            All it does it just generate a very large text file
---
+++ StorageGeneratorV2/textgenerator.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+# Define the file path and size in bytes
+file_path = "output.txt"
+file_size = 1 * 1024 * 1024  # 1 MB
+
+# Define the characters to use in the text
+characters = string.ascii_letters + string.digits
+
+# Open the file in append mode
+with open(file_path, "a") as f:
+    # Loop until the file reaches the desired size
+    while f.tell() < file_size:
+        # Generate a random string of characters
+        text = ''.join(random.choice(characters) for _ in range(1024))
+        # Append the text to the file
+        f.write(text)
