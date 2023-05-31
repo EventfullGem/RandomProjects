5e7abc0ee867ae522d1a9ebaace4f5c3809b1474
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-05-31 18:19:45 -0400

    Testing new IDE

--- ComparingLanguageSpeeds/speedtest.py
+++ ComparingLanguageSpeeds/speedtest.py
@@ -9,6 +9,7 @@ end_time = time.time()
 
 print("Time taken:", end_time - start_time, "seconds")
 
-#Time taken: 48.79 seconds
-#So when I run it in Visual Studio IDE and not Visual studio Code I get 7 seconds???
-#I dont know what im doing anymore
+# Time taken: 48.79 seconds
+# So when I run it in Visual Studio IDE and not Visual studio Code I get 7 seconds???
+# When I run it in JetBrains IDE I get 4.48 seconds?
+# I don't know what im doing anymore
--- Math Stuff/converter.py
+++ Math Stuff/converter.py
@@ -1,16 +1,16 @@
 
-#Only works on Linux I think, I still need to test this
-#As far as I know, does not work
-#If you somehow see this and want to fork it, please try and make it work :)
+# Only works on Linux I think, I still need to test this
+# As far as I know, does not work
+# If you somehow see this and want to fork it, please try and make it work :)
 
 
 
-#This code converts Ti-Basic to Python
-#This code is not perfect, but it works
+# This code converts Ti-Basic to Python
+# This code is not perfect, but it works
 from ti842py import transpile
 
 transpile("clock.txt", "tiprogram.py")
-#clock.txt is the Ti-Basic (input) program
-#tiprogram.py is the Python (output) program
+# clock.txt is the Ti-Basic (input) program
+# tiprogram.py is the Python (output) program
 
 
