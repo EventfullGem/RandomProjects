d7a2eb59cd0bb88237922700fbe2db5a62ae6ece
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-01 15:26:25 -0400

    Creating a graphing script in python

--- Math Stuff/converter.py
+++ Math Stuff/converter.py
@@ -1,16 +1,1 @@
-
-# Only works on Linux I think, I still need to test this
-# As far as I know, does not work
-# If you somehow see this and want to fork it, please try and make it work :)
-
-
-
-# This code converts Ti-Basic to Python
-# This code is not perfect, but it works
-from ti842py import transpile
-
-transpile("clock.txt", "tiprogram.py")
-# clock.txt is the Ti-Basic (input) program
-# tiprogram.py is the Python (output) program
-
-
+#Testing graphing on this now
