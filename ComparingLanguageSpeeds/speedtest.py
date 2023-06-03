d3bd71914645cbc0ec45cae76722764869d1f6c7
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-03 12:37:23 -0400

    Improving speedtest.py

--- ComparingLanguageSpeeds/speedtest.py
+++ ComparingLanguageSpeeds/speedtest.py
@@ -2,7 +2,7 @@ import time
 
 start_time = time.time()
 
-for i in range(1000000):
+for i in range(1000000000):
     print(i)
 
 end_time = time.time()
@@ -12,4 +12,8 @@ print("Time taken:", end_time - start_time, "seconds")
 # Time taken: 48.79 seconds
 # So when I run it in Visual Studio IDE and not Visual studio Code I get 7 seconds???
 # When I run it in JetBrains IDE I get 4.48 seconds?
+# ran on my server I get ... 4.49 seconds
 # I don't know what im doing anymore
+
+
+# My server can count to one billion in about ... didnt finish, I'm trying to test multi core support for python now
