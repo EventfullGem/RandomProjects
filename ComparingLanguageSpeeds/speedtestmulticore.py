817f5e403fd6312e78086b65c242cdc3e7e8aa93
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-04 14:18:19 -0400

    Added comments to speedtestmulticore

--- ComparingLanguageSpeeds/speedtestmulticore.py
+++ ComparingLanguageSpeeds/speedtestmulticore.py
@@ -3,13 +3,14 @@ import time
 
 def worker():
     # worker function
+    # function that prints a message and a
     print('Worker')
     x = 0
     while x < 1000:
         print(x)
         x += 1
     return
-
+# Create 50 processes and start them
 if __name__ == '__main__':
     start_time = time.time()
     jobs = []
@@ -17,10 +18,10 @@ if __name__ == '__main__':
         p = multiprocessing.Process(target=worker)
         jobs.append(p)
         p.start()
-
+# Wait for all processes to finish
     for job in jobs:
         job.join()
 
     end_time = time.time()
-
+# Calculate the time taken and outputs to console window
     print("Time taken:", end_time - start_time, "seconds")
