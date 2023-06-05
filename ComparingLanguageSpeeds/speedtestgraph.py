cc3338dedb7ea5c2d3fb639e7d08009e0f3e7405
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-04 21:00:04 -0400

    Finalized Speed Graphs
    Added custom legend, organized the chart, and added the PNG of the chart

---
+++ ComparingLanguageSpeeds/speedtestgraph.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
+
+left = [1, 2, 3, 4, 5]
+height = [1, 12.8, 3.67, .12, .22]
+
+tick_label = ['Linux Kernel', 'Windows CMD', 'IntelliJ IDEA', 'WSL', 'Linux Kernel']
+
+# plots the bar chart
+plt.bar(left, height, tick_label=tick_label,
+        width=0.8, color=['#7DADD8', '#9C7DD8', '#9C7DD8', '#9C7DD8', '#7DADA8' ])
+
+# naming the x-axis
+plt.xlabel('Test Environment')
+# naming the y-axis
+plt.ylabel('Time to complete (seconds)')
+# plot title
+plt.title('Multi Core Speed Tests')
+
+# create custom legend
+legend_elements = [Line2D([0], [0], color='#7DADD8', lw=4, label='Raspberry Pi 4B 8GB'),
+                   Line2D([0], [0], color='#9C7DD8', lw=4, label='i7-10750H CPU @ 2.6GHz'),
+                   Line2D([0], [0], color='#7DADA8', lw=4, label='Xeon E5-645 v5 CPU @ 2.4GHz')]
+
+# add legend to plot
+plt.legend(handles=legend_elements)
+
+# function to show the plot
+plt.show()
