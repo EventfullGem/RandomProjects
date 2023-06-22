d97ff15a4215f44431252037e9e63b1d6bef0561
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-21 21:27:47 -0400

    Delete test.r
    Couldn't find out how to get R to work

--- Math Stuff/test.r
+++
@@ -1,9 +0,0 @@
-# Load the ggplot2 package
-library(ggplot2)
-
-# Create a data frame with x and y values
-df <- data.frame(x = c(1, 2, 3, 4, 5), y = c(2, 4, 5, 4, 5))
-
-# Create a scatter plot with x and y values
-ggplot(df, aes(x = x, y = y)) + 
-  geom_point()
